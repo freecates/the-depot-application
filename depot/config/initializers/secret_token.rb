6c9c92217b368cf2bd1f09250651ed4010056f81
Ramon <ramongil@ramongil.cat>
2014-01-28 22:23:34 +0100

    set up Depot app as a Rails project. Created the development database and configured app to connect to it. Created a products controller and a Product model.

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '378411080c8e994a9af9c02ec7a70e6f9b372e7703b786d304940fa3d2b73fb50ddc210b48292711e11c401904ed484db9636ddca275f16976ab0b2086e961d4'
